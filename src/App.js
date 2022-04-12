03b32bac464d597f121227317e483132627b386c
Ashton B <ashtonbrugh@gmail.com>
2022-04-12 16:11:38 -0400

    enabling chat

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed';
+
 
 const App = () => {
     return (
@@ -9,7 +11,7 @@ const App = () => {
             projectID="8f2e3079-2e31-42e3-8ae7-dd4784e9f48f"
             userName="javascriptmastery"
             userSecret="123123"
-
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} /> }
         />
     )
 }
